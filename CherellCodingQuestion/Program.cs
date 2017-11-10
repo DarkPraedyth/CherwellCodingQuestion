6f65cbfb114ac7c65fce4f79448f7ea2b19da4cb
ab cd <drface007@outlook.com>
2017-11-10 01:15:50 -0600

    Add project files.

---
+++ CherellCodingQuestion/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CherellCodingQuestion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle triangle = new Triangle();
+
+            // Test with A1, C6, E7, F12
+            // get the coordinates from the row and columns
+            Coordinates coords1 = triangle.GetCoordinates(RowLetter.A, 1);
+            Coordinates coords2 = triangle.GetCoordinates(RowLetter.C, 6);
+            Coordinates coords3 = triangle.GetCoordinates(RowLetter.E, 7);
+            Coordinates coords4 = triangle.GetCoordinates(RowLetter.F, 12);
+
+            // output the results.
+            Console.WriteLine(string.Format("A1: v1({0},{1}), v2({2},{3}), v3({4},{5})",
+                coords1.V1.X, coords1.V1.Y, coords1.V2.X, coords1.V2.Y, coords1.V3.X, coords1.V3.Y));
+
+            Console.WriteLine(string.Format("C6: v1({0},{1}), v2({2},{3}), v3({4},{5})",
+                coords2.V1.X, coords2.V1.Y, coords2.V2.X, coords2.V2.Y, coords2.V3.X, coords2.V3.Y));
+
+            Console.WriteLine(string.Format("E7: v1({0},{1}), v2({2},{3}), v3({4},{5})",
+                coords3.V1.X, coords3.V1.Y, coords3.V2.X, coords3.V2.Y, coords3.V3.X, coords3.V3.Y));
+
+            Console.WriteLine(string.Format("F12: v1({0},{1}), v2({2},{3}), v3({4},{5})",
+                coords4.V1.X, coords4.V1.Y, coords4.V2.X, coords4.V2.Y, coords4.V3.X, coords4.V3.Y));
+
+            // create the coordinates for A1, C6, E7, F12
+            Coordinates coordA1 = new Coordinates();
+            coordA1.V1.X = 0;
+            coordA1.V1.Y = 10;
+            coordA1.V2.X = 0;
+            coordA1.V2.Y = 0;
+            coordA1.V3.X = 10;
+            coordA1.V3.Y = 10;
+
+            Coordinates coordC6 = new Coordinates();
+            coordC6.V1.X = 30;
+            coordC6.V1.Y = 20;
+            coordC6.V2.X = 20;
+            coordC6.V2.Y = 20;
+            coordC6.V3.X = 30;
+            coordC6.V3.Y = 30;
+
+            Coordinates coordE7 = new Coordinates();
+            coordE7.V1.X = 30;
+            coordE7.V1.Y = 50;
+            coordE7.V2.X = 30;
+            coordE7.V2.Y = 40;
+            coordE7.V3.X = 40;
+            coordE7.V3.Y = 50;
+
+            Coordinates coordF12 = new Coordinates();
+            coordF12.V1.X = 60;
+            coordF12.V1.Y = 50;
+            coordF12.V2.X = 50;
+            coordF12.V2.Y = 50;
+            coordF12.V3.X = 60;
+            coordF12.V3.Y = 60;
+
+            // get the row and column for those coordinates and verify they are corrctly retrieved
+            RowAndColumn rowColA1 = triangle.GetRowAndColumn(coordA1);
+            RowAndColumn rowColC6 = triangle.GetRowAndColumn(coordC6);
+            RowAndColumn rowColE7 = triangle.GetRowAndColumn(coordE7);
+            RowAndColumn rowColF12 = triangle.GetRowAndColumn(coordF12);
+
+            // output the results
+            Console.WriteLine(rowColA1.Row.ToString() + rowColA1.Column);
+            Console.WriteLine(rowColC6.Row.ToString() + rowColC6.Column);
+            Console.WriteLine(rowColE7.Row.ToString() + rowColE7.Column);
+            Console.WriteLine(rowColF12.Row.ToString() + rowColF12.Column);
+
+            Console.Read();
+        }
+    }
+}
---
+++ CherellCodingQuestion/Triangle.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// TODO: validation for input values for coordinates or rows/columns
+// TODO: validation for input values for coordinates to check if valid triangle coordinates
+
+namespace CherellCodingQuestion
+{
+    // each vertex v1, v2, v3 will be a Vector
+    public struct Vector
+    {
+        public int X;
+        public int Y;
+
+        public Vector(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    // each coordinate wil contain 3 vectors
+    public struct Coordinates
+    {
+        public Vector V1;
+        public Vector V2;
+        public Vector V3;
+    }
+
+    // row and column representation of the triangle
+    public class RowAndColumn
+    {
+        public RowLetter Row { get; set; }
+        public int Column { get; set; }        
+    }
+
+    // The rows
+    public enum RowLetter
+    {
+        A = 1,
+        B = 2,
+        C = 3,
+        D = 4,
+        E = 5,
+        F = 6
+    }
+
+    public class Triangle
+    {     
+        public RowLetter RowLetter { get; set; }
+
+        public int Column { get; set; }
+        public int RowNum { get; set; }
+
+        public Coordinates Coordinates { get; set; }
+
+        // get coordinates for the provided row and column
+        public Coordinates GetCoordinates(RowLetter rowLetter, int column)
+        {
+            int row = Convert.ToInt32(rowLetter);            
+
+            Coordinates coordinates = new Coordinates();
+                        
+            // v2 is always the top left point whether it is an even or odd column.
+            coordinates.V2.Y = (row - 1) * 10;
+            coordinates.V2.X = ((column-1) / 2) * 10;
+
+            // v3 is always the bottom right point whether it is an even or odd column.
+            coordinates.V3.Y = row * 10;
+            coordinates.V3.X = coordinates.V2.X + 10;
+
+            // use this check to properly set the v1
+            if (isUpperTriangle(rowLetter, column))
+            {
+                // will be top right point if upper triangle
+                coordinates.V1.Y = coordinates.V2.Y;
+                coordinates.V1.X = coordinates.V3.X;
+            }
+            else
+            {
+                // will be bottom left point if lower triangle
+                coordinates.V1.Y = coordinates.V3.Y;
+                coordinates.V1.X = coordinates.V2.X;
+            }
+
+            return coordinates;
+        }
+
+        // get row and column for provided coordinates
+        public RowAndColumn GetRowAndColumn(Coordinates coordinates)
+        {
+            RowLetter row;
+            int column;
+
+            //use this check to properly set the vertex
+            if (isUpperTriangle(coordinates))
+            {
+                row = (RowLetter)(coordinates.V3.Y / 10);
+                column = ((coordinates.V2.X / 10) * 2) + 2;
+            }
+            else
+            {
+                row = (RowLetter)(coordinates.V1.Y / 10);
+                column = ((coordinates.V2.X / 10) * 2) + 1;
+            }            
+
+            RowAndColumn rowAndColumn = new RowAndColumn();
+            rowAndColumn.Row = row;
+            rowAndColumn.Column = column;
+
+            return rowAndColumn;
+        }
+
+        // assumption, the top triangle's (even columns) v2 and v3 are same point as shown in image in pdf
+        // and v1 is the point where the non-hypotenuse lines meet
+
+        // if column is even, then upper
+        private bool isUpperTriangle(RowLetter rowLetter, int column)
+        {
+            return (column % 2 == 0);            
+        }
+
+        // if v1y is same as v2y, then upper
+        private bool isUpperTriangle(Coordinates coordinates)
+        {
+            return (coordinates.V1.Y == coordinates.V2.Y);            
+        }
+    }
+}
